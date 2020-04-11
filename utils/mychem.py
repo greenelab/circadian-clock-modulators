57b7bfde21b31833d863e818d25fb7f64ea34e25
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-04-11 09:28:04 -0400

    move pubchem and mychem funtions to modules

---
+++ utils/mychem.py
@@ -0,0 +1,39 @@
+import re
+
+import requests
+
+
+def solr_escape(text):
+    """
+    Escape reserved characters for pysolr queries.
+    https://lucene.apache.org/solr/guide/6_6/the-standard-query-parser.html#TheStandardQueryParser-EscapingSpecialCharacters
+    https://docs.mychem.info/en/latest/doc/chem_query_service.html#escaping-reserved-characters
+    """
+    import re
+    reserved_chars = '+ - = && || > < ! ( ) { } [ ] ^ " ~ * ? : \ /'.split()
+    pattern = re.compile('|'.join(map(re.escape, reserved_chars)))
+    return pattern.sub(repl= lambda m: f"\\{m.group()}", string=text)
+
+
+def get_mychem_query(name):
+    """
+    https://lucene.apache.org/solr/guide/6_6/the-standard-query-parser.html
+    """
+    escaped_name = solr_escape(name)
+    search_fields = ["drugbank.name", "drugbank.synonyms"]
+    query = " OR ".join(f'{field}:"{escaped_name}"' for field in search_fields)
+    query = f"_exists_:drugbank AND ({query})"
+    return query
+
+
+def query_mychem(name):
+    url = "https://mychem.info/v1/query"
+    params = dict(
+        q=get_mychem_query(name),
+        fields="drugbank.name,drugbank.id,drugbank.synonyms,drugbank.formula,drugbank.weight",
+        size=1,
+    )
+    response = requests.get(url, params)
+    # solr doesn't have a whole target string match mode.
+    # For example, "Gallic acid" will match "gallic acid bismuth basic salt"
+    return response
---
+++ utils/pubchem.py
@@ -0,0 +1,40 @@
+import re
+import logging
+
+import requests
+
+
+def query_pubchem(compound_name):
+    """
+    https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Pharmakon1600-01500621/xrefs/SBURL/JSON
+    """
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{compound_name}/xrefs/SBURL/JSON"
+    response = requests.get(url)
+    if not response.ok:
+        logging.info(f"query for {compound_name} returned status code {response.status_code}\n{response.url}")
+        return None
+    return response.json()
+
+
+def url_to_drugbank_id(url):
+    pattern = re.compile(
+        r"https?://(www.)?drugbank.ca/drugs/(?P<drugbank_id>DB[0-9]+)")
+    match = pattern.match(url)
+    if match:
+        return match.group("drugbank_id")
+
+
+def query_pubchem_for_drugbank(compound_name):
+    results = query_pubchem(compound_name)
+    if not results:
+        return None
+    results = results["InformationList"]["Information"][0]
+    drugbank_ids = sorted(set(filter(None, map(url_to_drugbank_id, results["SBURL"]))))
+    if len(drugbank_ids) > 1:
+        logging.info(f"{compound_name} maps to multiple DrugBank IDs: {drugbank_ids}")
+    row = dict(
+        query_compound=compound_name,
+        pubchem_cid=results["CID"],
+        drugbank_id=drugbank_ids[0] if drugbank_ids else None,
+    )
+    return row
