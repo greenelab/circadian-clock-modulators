7e03a714595fa086d958aa8c9ed1795f7995eafd
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2020-04-15 20:38:53 -0400

    map screenwell fda compounds to pubchem
    closes https://github.com/greenelab/circadian-clock-modulators/issues/1

--- utils/pubchem.py
+++ utils/pubchem.py
@@ -1,17 +1,21 @@
-import re
 import logging
+import re
+import urllib.parse
 
 import requests
 
 
-def query_pubchem(compound_name):
+def query_pubchem(compound, by="name"):
     """
+    by: either "name" or "smiles"
+    https://pubchemdocs.ncbi.nlm.nih.gov/pug-rest-tutorial$_Toc458584416
     https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/Pharmakon1600-01500621/xrefs/SBURL/JSON
     """
-    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{compound_name}/xrefs/SBURL/JSON"
+    compound_quoted = urllib.parse.quote(compound, safe='')
+    url = f"https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/{by}/{compound_quoted}/xrefs/SBURL/JSON"
     response = requests.get(url)
     if not response.ok:
-        logging.info(f"query for {compound_name} returned status code {response.status_code}\n{response.url}")
+        logging.info(f"query for {compound} returned status code {response.status_code}\n{response.url}")
         return None
     return response.json()
 
@@ -24,16 +28,16 @@ def url_to_drugbank_id(url):
         return match.group("drugbank_id")
 
 
-def query_pubchem_for_drugbank(compound_name):
-    results = query_pubchem(compound_name)
+def query_pubchem_for_drugbank(compound, by="name"):
+    results = query_pubchem(compound, by=by)
     if not results:
         return None
     results = results["InformationList"]["Information"][0]
     drugbank_ids = sorted(set(filter(None, map(url_to_drugbank_id, results["SBURL"]))))
     if len(drugbank_ids) > 1:
-        logging.info(f"{compound_name} maps to multiple DrugBank IDs: {drugbank_ids}")
+        logging.info(f"{compound} maps to multiple DrugBank IDs: {drugbank_ids}")
     row = dict(
-        query_compound=compound_name,
+        query_compound=compound,
         pubchem_cid=results["CID"],
         drugbank_id=drugbank_ids[0] if drugbank_ids else None,
     )
